3cbe3c6fc42a549f880253f9717b0d1f02c6225b
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-03-23 18:52:11 +0600

    update some css

--- src/Pages/Home/Genres/MovieListByGenres.jsx
+++ src/Pages/Home/Genres/MovieListByGenres.jsx
@@ -44,7 +44,7 @@ const MoviesByGenres = () => {
           No movies found in this genre.
         </p>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-5">
           {movies.map((movie) => (
             <MovieCard key={movie._id} movie={movie} />
           ))}
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -57,7 +57,7 @@ const Navbar = () => {
         {/* mobile menu */}
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex={0} role="button" className="btn btn-ghost text-white lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
