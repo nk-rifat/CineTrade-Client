2849c4d0c26aca781d446835266b9ed293366ba5
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-03-26 07:26:34 +0600

    write a private route component

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <span className="loading loading-spinner loading-lg text-primary"></span>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+};
+
+export default PrivateRoute;
