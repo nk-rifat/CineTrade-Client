fb2d0837b10e8f5567fda4825f80f0dd1a6939d8
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-04-20 18:46:49 +0600

    make DashboardHome component

--- src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome/DashboardHome.jsx
@@ -1,9 +1,18 @@
+import useCurrentUser from "../../hooks/useCurrentUser";
+import UserHome from "./UserHome";
+import PartnerHome from "./PartnerHome";
+import AdminHome from "./AdminHome";
+
 const DashboardHome = () => {
-    return (
-        <div>
-            Dashboard Home
-        </div>
-    );
+  const { data: currentUser, isLoading } = useCurrentUser();
+
+  if (isLoading) return <span className="loading loading-spinner"></span>;
+
+  const role = currentUser?.role;
+
+  if (role === "admin") return <AdminHome />;
+  if (role === "partner") return <PartnerHome />;
+  return <UserHome />;
 };
 
 export default DashboardHome;
