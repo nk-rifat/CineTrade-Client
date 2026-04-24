c8ff33384b071902c34ca540e8d7f738176ff6eb
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-04-24 15:25:34 +0600

    use gradient color on movie title of banner section

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -48,7 +48,10 @@ const Banner = () => {
               {/* --- CONTENT LAYER --- */}
               <div className="relative h-full max-w-7xl mx-auto px-6 md:px-12 flex flex-col justify-center items-center md:items-end text-center md:text-right text-white z-10">
                 <div className="max-w-2xl space-y-4 md:space-y-6">
-                  <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-black leading-tight tracking-tighter uppercase drop-shadow-2xl">
+                  <h1
+                    className="text-3xl sm:text-4xl md:text-6xl lg:text-8xl font-black leading-tight tracking-tighter uppercase drop-shadow-2xl 
+               bg-gradient-to-r from-sky-400 via-white to-amber-500 bg-clip-text text-transparent"
+                  >
                     {movie.title}
                   </h1>
 
