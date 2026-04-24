2eca05ff2251e758975d511ef77640eded7ef3f2
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-04-24 20:12:02 +0600

    update css

--- src/Components/Shared/Error.jsx
+++ src/Components/Shared/Error.jsx
@@ -1,17 +1,17 @@
 import { HiOutlineRefresh, HiOutlineExclamationCircle } from "react-icons/hi";
 
-const Error = ({ 
-  message = "Something went wrong while fetching data.", 
-  onRetry 
+const Error = ({
+  message = "Something went wrong while fetching data.",
+  onRetry,
 }) => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-[400px] p-6 text-center border border-red-500/20 bg-red-500/5 rounded-3xl">
+    <div className="flex flex-col items-center justify-center w-full h-[400px] p-6 text-center border border-red-500/20 bg-[#050505]">
       <HiOutlineExclamationCircle className="text-red-500 mb-4" size={50} />
-      <h3 className="text-xl font-bold text-white mb-2">Oops! Execution Error</h3>
-      <p className="text-zinc-400 text-sm max-w-xs mb-6">
-        {message}
-      </p>
-      
+      <h3 className="text-xl font-bold text-white mb-2">
+        Oops! Execution Error
+      </h3>
+      <p className="text-zinc-400 text-sm max-w-xs mb-6">{message}</p>
+
       {onRetry && (
         <button
           onClick={onRetry}
